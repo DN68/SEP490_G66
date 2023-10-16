359c86ee2c27a8051ca361bd3e69201c209400bb
AnPham <95533400+Oversoul1113@users.noreply.github.com>
2023-10-16 17:29:16 +0700

    Setting up DB Connection

---
+++ Prolancer/src/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const { connectToDb, getDb } = require('./db')
+const { ObjectId } = require('mongodb')
+
+// init app & middleware
+const app = express()
+app.use(express.json())
+
+let db
+
+//db connection
+connectToDb((err) => {
+    if (!err) {
+        // port number
+        app.listen(3000, () => {
+            console.log('app listening on port 3000')
+        })
+        db = getDb()
+    }
+})
+
+app.get('/users', (req, res) => {
+    res.json({ mssg: "welcome to the api" })
+})
---
+++ Prolancer/src/db.js
@@ -0,0 +1,19 @@
+const { MongoClient } = require('mongodb')
+
+let dbConnection
+
+module.exports = {
+    connectToDb: (cb) => {
+        MongoClient.connect('mongodb://127.0.0.1:27017/Prolancer')
+        // MongoClient.connect('mongodb+srv://anpqhe160968:Pqa13112002@cluster0.6ac0vz8.mongodb.net/')
+            .then((client) => {
+                dbConnection = client.db()
+                return cb()
+            })
+            .catch(err => {
+                console.log(err)
+                return cb(err)
+            })
+    },
+    getDb: () => dbConnection
+}
