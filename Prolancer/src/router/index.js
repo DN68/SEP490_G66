1e7ffda3fb3a9f79b07f52ca26e4b3a1d623e1f3
Oversoul1113 <anpqhe160968@fpt.edu.vn>
2023-12-03 09:56:24 +0700

    library update

--- Prolancer/src/router/index.js
+++ Prolancer/src/router/index.js
@@ -339,6 +339,8 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/popup.vue')
+  },
+  {
     path: '/vieworderrequestdetail/:id',
     name: 'vieworderrequestdetail',
     // route level code-splitting
