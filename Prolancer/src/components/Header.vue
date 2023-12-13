5f981741454e168cee17eb315cd28bbf4f7579ec
anhlt190101 <anhlt190101@gmail.com>
2023-12-13 23:17:45 +0700

    add library

--- Prolancer/src/components/Header.vue
+++ Prolancer/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light ">
       <!-- Container wrapper -->
       <div class="container">
         <!-- Navbar brand -->
--- Prolancer/src/main.js
+++ Prolancer/src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import "chart.js";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import * as jQuery from 'jquery';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'jquery';
+// Make jQuery available globally
+window.jQuery = window.$ = jQuery;
 createApp(App).use(router).mount('#app')
